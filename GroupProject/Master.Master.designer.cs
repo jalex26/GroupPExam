c2b916a9dc5e177148b6bbf07658f987a6a731de
acarter032908 <adrian.carter@robertsoncollege.net>
2014-10-07 14:58:13 -0500

    xml navigation

--- GroupProject/Master.Master.designer.cs
+++ GroupProject/Master.Master.designer.cs
@@ -31,6 +31,15 @@ namespace GroupProject {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// Xml1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Xml Xml1;
+        
+        /// <summary>
         /// pnlLogin control.
         /// </summary>
         /// <remarks>
