605b667ac2437feb929d8f0c9a5bc11c5e53317b
acarter032908 <adrian.carter@robertsoncollege.net>
2014-10-23 13:29:29 -0500

    nothing

---
+++ GroupProject/QuizToSession.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace GroupProject
+{
+    public class QuizToSession
+    {
+        List<XMLQuizContent> Quiz = new List<XMLQuizContent>();
+
+        public QuizToSession()
+        {
+            if (HttpContext.Current.Session["Quiz"] !=null)
+            {
+                Quiz = (List<XMLQuizContent>)HttpContext.Current.Session["Quiz"];
+            }
+        }
+        public void Save()
+        {
+            HttpContext.Current.Session["Quiz"] = Quiz;
+        }
+    }
+}
---
+++ GroupProject/XMLQuizContent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupProject
+{
+    public class XMLQuizContent
+    {
+        public int XMLQuizID { get; set; }
+        public string Title { get; set; }
+        public string Subject { get; set; }
+        public int CourseID { get; set;}
+        public int Time { get; set; }
+        public int DifficultyId { get; set; }
+    }
+}
