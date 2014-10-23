03632ff3595a04d0122b8a3c80347166c10aab9b
bebzamor <veberly.carvalho@robertsoncollege.net>
2014-10-23 15:02:14 -0500

    All

---
+++ GroupProject/LoadBoxes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DAL_Project;
+using System.Data;
+
+
+namespace GroupProject
+{
+    public class LoadBoxes
+    {
+        DAL myDal = new DAL(Globals.conn);
+
+        public DataSet LoadCourse()
+        {
+            myDal.ClearParams();
+            DataSet ds = myDal.ExecuteProcedure("spLoadCourse");
+            return ds;
+        }
+
+        public DataSet LoadClasses(string CourseId)
+        {
+            myDal.ClearParams();
+            myDal.AddParam("@CourseId", CourseId);
+            DataSet ds = myDal.ExecuteProcedure("spLoadClass");
+            return ds;
+        }
+    }
+}
