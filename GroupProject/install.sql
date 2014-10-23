a03a11602ec2c4aacb6efc0a73ea2d4440975492
kvncoliat <kvncoliat@gmail.com>
2014-10-22 19:32:05 -0500

    this one

--- GroupProject/install.sql
+++ GroupProject/install.sql
@@ -98,207 +98,9 @@ TotalScore decimal(10,5)
 )
 go
 
-<<<<<<< HEAD
-insert into tbResults(Userid,Versionid,Quizid,TotalScore)values 
-(2,1,0,85.50),(3,1,0,90.00),(4,1,0,90.95),(5,1,0,99.9)
-
---create table tbQuizTaken(
---QuizTakenid int primary key identity(0,1),
---Quizid int foreign key references tbQuiz(Quizid),
---QuizClass int foreign key references tbClass(Classid)
---)
---go
-
---insert into tbQuizTaken(Quizid,QuizClass)values
---(0,0),(0,1),(0,2),(0,3),(0,4)
---go
-
-create table tbQuizTaker(
-Takerid int primary key identity(0,1),
-Quizid int foreign key references tbQuiz(Quizid),
-Userid int foreign key references tbUser(Userid),
-Status int,
-Versionid int foreign key references tbQuizVersion(Versionid),
-DateAndTime Datetime
-)
-go
-
---1-scheduled to take
---2-Rescheduled
---3-Not taken
-
-insert into tbQuizTaker(Quizid,Userid,Status,Versionid,DateAndTime)values
-(1,2,1,1,'2014-01-26'),(1,3,2,1,'2014-03-14'),(1,4,3,1,'2014-05-13'),(1,5,3,1,'2014-07-01')
-go
-
-create table tbMultipleQuestions(
-MultipleQuestionsid int primary key identity (0,1),
-Question varchar(max),
-Choice1 varchar(150),
-Choice2 varchar(150),
-Choice3 varchar(150),
-Choice4 varchar(150),
-Answer varchar(150),
-Versionid int foreign key references tbQuizVersion(Versionid)
-)
-go
-
-insert into tbMultipleQuestions(Question,Choice1,Choice2,Choice3,Choice4,Answer,Versionid)values
-('What is the capital of Canada?','Montreal','Ontario','Ottawa','Winnipeg','Ottawa',1),
-('How old is the legal age to drink in Canada?','17','18','19','20','18',1),
-('How many colors are there in a rainbow?','4','6','9','7','7',1),
-('When is Remembrance Day celebrated?','July 1st','October 1st','November 11th','December 25th','November 11th',1),
-('How many sides are there in an Octagon','6','12','4','8','8',1),
-('If I say seventy five is the correct answer to this question but I say it backwards, what is the correct answer to this question?',
-	'75','Seventy Five','evif ytneves','Is this a trick question?','evif ytneves',1)
-go
-
-create table tbMatchingQuestions(
-MatchingQuestionsid int primary key identity(0,1),
-Question varchar(150),
-Choices varchar(150),
-Answers varchar(150),
-Versionid int foreign key references tbQuizVersion(Versionid)
-)
-go
-
---Match the City with the Country
-insert into tbMatchingQuestions(Question,Choices,Answers,Versionid)values
-('London','France','Great Britain',1),
-('Paris','Russia','France',1),
-('Athens','Great Britain','Greece',1),
-('Moscow','Spain','Russia',1),
-('Madrid','Greece','Spain',1)
-go
-
-create table tbLongQuestions(
-LongQuestionsid int primary key identity(0,1),
-Question varchar(150),
-Answer varchar(max),
-Versionid int foreign key references tbQuizVersion(Versionid)
-)
-go
-
-insert into tbLongQuestions(Question,Answer,Versionid)values
-('What is Equilibrium?','State of stable conditions in which all significant factors remain more or less constant over a period, and there is little or no inherent tendency for change.',1),
-('What is time?','Time is the fourth dimension and a measure in which events can be ordered from the past through the present into the future, and also the measure of durations of events and the intervals between them.',1),
-('Why do we need sleep?','Sleep gives your body a rest and allows it to prepare for the next day.',1),
-('Which has more power, love or fear?','Love. Fear will only have people obeying you until they can get away. Love will have people willing to die for each other and for you.',1),
-('What is Science?','the intellectual and practical activity encompassing the systematic study of the structure and behavior of the physical and natural world through observation and experiment.',1)
-go
-
-create table tbTrueOrFalseQuestions(
-TrueOrFalseQuestionsid int primary key identity(0,1),
-Question varchar(150),
-True varchar(60),
-False varchar(60),
-Answers varchar(60),
-Versionid int foreign key references tbQuizVersion(Versionid)
-)
-go
-
-insert into tbTrueOrFalseQuestions(Question,True,False,Answers,Versionid)values
-('In the weightlessness of space, if a frozen pea touches pepsi it will blow up.','True','False','True',1),
-('The worlds smartest pig memorized the multiplication table up to 12.','True','False','True',1),
-('Monkeys are related to fish because if need be they can breathe underwater.','True','False','False',1),
-('Men are 4 times more likely to be struck by lightening than women','True','False','True',1),
-('In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.','True','False','True',1)
-go
-
-create table tbFInBlanksQuestion(
-FInBlanksid int primary key identity(0,1),
-Question varchar(150),
-Answers varchar(150),
-Versionid int foreign key references tbQuizVersion(Versionid)
-)
-go
-
-insert into tbFInBlanksQuestion(Question,Answers,Versionid)values 
-('Walt Disney, creator of Mickey Mouse was afraid of _____.','mice',1),
-('Most dust particles in your house are made from dead _____.','skin',1),
-('The chances of you dying on the way to the lottery tickets is ______ than your chances of winning','greater',1),
-('Koalas sleep for ___ hours a day','22',1),
-('Sea ______ hold hands in their sleep so they dont drift away from each other','otters',1)
-go
-
---User's Answers
-create table tbMultipleAnswers(
-MultipleAnswersid int primary key identity(0,1),
-Userid int foreign key references tbUser(Userid),
-MultipleQuestionsid int foreign key references tbMultipleQuestions(MultipleQuestionsid),
-UserAnswer varchar(150)
-)
-
-go
-
-insert into tbMultipleAnswers(Userid,MultipleQuestionsid,UserAnswer)values
-(3,0,'Ottawa'),(3,1,'17'),(3,2,'7'),(3,3,'November 11th'),(3,4,'8')
-go
-
---User's Answers
-create table tbMatchingAnswers(
-MatchingAnswersid int primary key identity(0,1),
-MatchingQuestionsid int foreign key references tbMatchingQuestions(MatchingQuestionsid),
-Userid int foreign key references tbUser(Userid),
-Quizid int foreign key references tbQuiz(Quizid),
-UserAnswer varchar(150)
-)
-go
-
-insert into tbMatchingAnswers(MatchingQuestionsid,Userid,Quizid,UserAnswer)values
-(0,4,1,'Great Britain'),(1,4,1,'France'),(2,4,1,'Greece'),(3,4,1,'Russia'),(4,4,1,'Spain')
-go
-
---User's Answers
-create table tbLongAnswers(
-LongAnswersid int primary key identity (0,1),
-Userid int foreign key references tbUser(Userid),
-LongQuestionsid int foreign key references  tbLongQuestions(LongQuestionsid),
-Quizid int foreign key references tbQuiz(Quizid),
-UserAnswer varchar(max)
-)
-go
-
-insert into tbLongAnswers(Userid,LongQuestionsid,Quizid,UserAnswer)values
-(5,0,1,'State of stable conditions in which all significant factors remain more or less constant over a period, and there is little or no inherent tendency for change.'),
-(5,1,1,'Time is the fourth dimension and a measure in which events can be ordered from the past through the present into the future, and also the measure of durations of events and the intervals between them.'),
-(5,2,1,'Sleep gives your body a rest and allows it to prepare for the next day.'),
-(5,3,1,'Love. Fear will only have people obeying you until they can get away. Love will have people willing to die for each other and for you.'),
-(5,4,1,'the intellectual and practical activity encompassing the systematic study of the structure and behavior of the physical and natural world through observation and experiment.')
-go
-
---User's Answers
-create table tbTrueOrFalseAnswers(
-TrueOrFalseAnswersid int primary key identity(0,1),
-Userid int foreign key references tbUser(Userid),
-TrueOrFalseQuestionsid int foreign key references tbTrueOrFalseQuestions(TrueOrFalseQuestionsid),
-UserAnswer varchar(150)
-)
-go
-
-insert into tbTrueOrFalseAnswers(Userid,TrueOrFalseQuestionsid,UserAnswer)values
-(3,0,'True'),(3,1,'True'),(3,2,'False'),(3,3,'True'),(3,4,'False')
-go
-
-
-
---User's Answers
-create table tbFInBlanksAnswers(
-FInBlanksAnswersid int primary key identity(0,1),
-Userid int foreign key references tbUser(Userid),
-FInBlanksQuestionid int foreign key references tbMultipleQuestions(MultipleQuestionsid),
-UserAnswer varchar(150)
-)
-go
-
-insert into tbFInBlanksAnswers(Userid,FInBlanksQuestionid,UserAnswer)values
-(3,0,'mice'),(3,1,'skin'),(3,2,'less'),(3,3,'20'),(3,4,'otters')
-go
-=======
 --insert into tbResults(Userid,Versionid,Quizid,TotalScore)values 
 --(2,1,0,85.50),(3,1,0,90.00),(4,1,0,90.95),(5,1,0,99.9)
 
->>>>>>> 1a717f304bc92739bf09fe74defa5c159a13c4d1
 --Failed Login Attempts
 create table tbFailedLoginAttempt(
 Username varchar(60),
@@ -358,22 +160,29 @@ go
 
 ----testing xml datatype here to save uploaded quizzes----plz don't delete yet// thanks Nupur
 create table tbXMLQuizContent(
-QuizID int primary key identity (1,1),
-XMLQuizID int,    --extracting it from the XML file    
+XMLQuizID int primary key,    --extracting it from the XML file    
 Title varchar(60),
 Subject varchar(60),
 Course varchar(60),
 Time int,
 Difficulty varchar(20),
-<<<<<<< HEAD
-XMLFileContent xml
-=======
 XmlFile xml
->>>>>>> 1a717f304bc92739bf09fe74defa5c159a13c4d1
 )
 go
 
 
+create table tbMultipleChoice(
+QuestionID int primary key identity (1,1),
+XMLQuestionID int,    -- get this id from xml file
+Question varchar(255),
+OptionOne varchar(60),
+OptionTwo varchar(60),
+OptionThree varchar(60),
+OptionFour varchar(60),
+CorrectAnswer varchar(20) null,
+XMLQuizID int foreign key references tbXMLQuizContent(XMLQuizID)
+)
+go
 
 ------------------------STORED PROCEDURES-----------------------
 
@@ -386,21 +195,12 @@ as begin
  insert into tbXMLQuizContent
  select 
  t.value('@QuizId','int') as XMLQuizID,    --attribute from xml file
-<<<<<<< HEAD
- t.value('(Details/Title/text())[1]','VARCHAR(60)') as Title,   
- t.value('(Details/Subject/text())[1]','VARCHAR(60)') as Subject,   
- t.value('(Details/Course/text())[1]','VARCHAR(60)') as Course,   
- t.value('(Details/Time/text())[1]','int') as Time,   
- t.value('(Details/Difficulty/text())[1]','VARCHAR(60)') as Difficulty,   
- @xml as XMLFileContent
-=======
  t.value('(ns:Details/ns:Title/text())[1]','VARCHAR(60)') as Title,   
  t.value('(ns:Details/ns:Subject/text())[1]','VARCHAR(60)') as Subject,   
  t.value('(ns:Details/ns:Course/text())[1]','VARCHAR(60)') as Course,   
  t.value('(ns:Details/ns:Time/text())[1]','int') as Time,   
  t.value('(ns:Details/ns:Difficulty/text())[1]','VARCHAR(60)') as Difficulty,
  @xml as XmlFile   
->>>>>>> 1a717f304bc92739bf09fe74defa5c159a13c4d1
  from
  @xml.nodes('/ns:Quiz')AS TempTable(t)
   select @@identity as 'XMLQuizID'
@@ -408,27 +208,26 @@ end
 go
 
 
---create procedure spinsertMultipleChoice(
---@xml xml
---)
---as begin
--- set nocount on;
--- --insert into tbMultipleChoice
--- select
--- t.value('@QuizId','int') as XMLQuizID,    --attribute from xml file
-
--- t.value('(/Questions/MultipleChoice/Question/@ID)[1]','int') as XMLQuestionID,   
-
--- t.value('(Questions/Questi/text())[1]','VARCHAR(255)') as Question,   
--- t.value('(Questions/MultipleChoice/Question/Options/Option/text())[1]','VARCHAR(60)') as OptionOne,   
--- t.value('(Questions/MultipleChoice/Question/Options/Option/text())[2]','VARCHAR(60)') as OptionTwo,   
--- t.value('(Questions/MultipleChoice/Question/Options/Option/text())[3]','VARCHAR(60)') as OptionThree, 
--- t.value('(Questions/MultipleChoice/Question/Options/Option/text())[4]','VARCHAR(60)') as OptionFour,   
--- t.value('(Questions/MultipleChoice/Question/Options/Option/Correct/text())[1]','VARCHAR(20)') as CorrectAnswer
--- from
--- @xml.nodes('/Quiz')AS TempTable(t)
---end
---go
+create procedure spinsertMultipleChoice(
+@xml xml
+)
+as begin
+ set nocount on;
+ insert into tbMultipleChoice
+ select
+ t.value('../@QuizId','int') as XMLQuizID,
+ --t.value('@ID','int') as XMLQuestionID,   
+ t.value('(Questions/MultipleChoice/Question/ID/text())[1]','INT') as XMLQuestionID,
+ t.value('(Questions/Questi/text())[1]','VARCHAR(255)') as Question,   
+ t.value('(Questions/MultipleChoice/Question/Options/Option/text())[1]','VARCHAR(60)') as OptionOne,   
+ t.value('(Questions/MultipleChoice/Question/Options/Option/text())[2]','VARCHAR(60)') as OptionTwo,   
+ t.value('(Questions/MultipleChoice/Question/Options/Option/text())[3]','VARCHAR(60)') as OptionThree, 
+ t.value('(Questions/MultipleChoice/Question/Options/Option/text())[4]','VARCHAR(60)') as OptionFour,   
+ t.value('(Questions/MultipleChoice/Question/Options/Option/Correct/text())[1]','VARCHAR(20)') as CorrectAnswer
+ from
+ @xml.nodes('/Quiz')AS TempTable(t)
+end
+go
 
 
 
