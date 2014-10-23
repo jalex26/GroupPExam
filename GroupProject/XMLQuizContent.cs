c9a25c6c5506838e40339a89621b634232f4eac0
acarter032908 <adrian.carter@robertsoncollege.net>
2014-10-23 14:26:37 -0500

    Save Session

---
+++ GroupProject/XMLQuizContent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupProject
+{
+    public class XMLQuizContent
+    {
+        public int XMLQuiz { get; set; }
+        public string Title { get; set; }
+        public string Subject { get; set; }
+        public int CourseID { get; set; }
+        public int Time { get; set; }
+        public int DifficultyId { get; set; }
+    }
+}
