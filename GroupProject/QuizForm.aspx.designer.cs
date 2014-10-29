87af99196632f775d7d8c36b2893d8f0c2bebcc7
kvncoliat <kvncoliat@gmail.com>
2014-10-29 12:01:02 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	GroupProject.v12.suo
    
    nothing

--- GroupProject/QuizForm.aspx.designer.cs
+++ GroupProject/QuizForm.aspx.designer.cs
@@ -20,23 +20,5 @@ namespace GroupProject {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
-        /// XmlDataSource1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.XmlDataSource XmlDataSource1;
-        
-        /// <summary>
-        /// GridView1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
     }
 }
