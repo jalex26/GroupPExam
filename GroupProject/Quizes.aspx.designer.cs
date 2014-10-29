97f07c289614e4bf48bae0845450986d7af97350
jalex26	email = janry.alex@robertsoncollege.net <janry.alex@robertsoncollege.net>
2014-10-29 11:00:47 -0500

    Deleted all XML Test Datas

--- GroupProject/Quizes.aspx.designer.cs
+++ GroupProject/Quizes.aspx.designer.cs
@@ -11,32 +11,5 @@ namespace GroupProject {
     
     
     public partial class Quizes {
-        
-        /// <summary>
-        /// XMLBINDINGTEST control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button XMLBINDINGTEST;
-        
-        /// <summary>
-        /// Button1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
-        
-        /// <summary>
-        /// Button2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button2;
     }
 }
