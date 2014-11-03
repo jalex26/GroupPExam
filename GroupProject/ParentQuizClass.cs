293146994b159f83536d701c4ba1a7a9f521a7a9
Nupur123 <nupur.singh@robertsoncollege.net>
2014-11-03 15:15:04 -0600

    13

---
+++ GroupProject/ParentQuizClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupProject
+{
+    public class ParentQuizClass: List<ParentQuizClass>
+    {
+        public int QuestionId { get; set; }
+        public int QuizId { get; set; }
+    }
+}
