ca236e9d6c8a69b2e8ff0d99abb4028e18ac6a4a
bebzamor <veberly.carvalho@robertsoncollege.net>
2014-11-10 14:42:52 -0600

    adding state cookies.cs for sorting
    SortColumn

--- GroupProject/StateCookies.cs
+++ GroupProject/StateCookies.cs
@@ -7,47 +7,47 @@ namespace GroupProject
 {
     public class StateCookies
     {
-        //public string SortColumn;
-        //public string Direction;
+        public string SortColumn;
+        public string Direction;
 
-        //public StateCookies()
-        //{
-        //    if (HttpContext.Current.Request.Cookies["StateCookies"] != null)
-        //    {
-        //        SortColumn = HttpContext.Current.Request.Cookies["StateCookies"]["SortColumn"];
-        //        Direction = HttpContext.Current.Request.Cookies["StateCookies"]["Direction"];
-        //    }
-        //    else
-        //    {
-        //        SortColumn = "FullName";
-        //        Direction = "desc";
-        //        Save();
-        //    }
-        //}
-        //public void Save()
-        //{
-        //    HttpContext.Current.Response.Cookies["StateCookies"]["SortColumn"] = SortColumn;
-        //    HttpContext.Current.Response.Cookies["StateCookies"]["Direction"] = Direction;
-        //}
-        //public void ColumnChange(string NewColumn)
-        //{
-        //    if (SortColumn == NewColumn)
-        //    {
-        //        if (Direction == "desc")
-        //        {
-        //            Direction = "asc";
-        //        }
-        //        else
-        //        {
-        //            Direction = "desc";
-        //        }
-        //    }
-        //    else
-        //    {
-        //        Direction = "asc";
-        //    }
-        //    SortColumn = NewColumn;
-        //    Save();
-        //}
+        public StateCookies()
+        {
+            if (HttpContext.Current.Request.Cookies["StateCookies"] != null)
+            {
+                SortColumn = HttpContext.Current.Request.Cookies["StateCookies"]["SortColumn"];
+                Direction = HttpContext.Current.Request.Cookies["StateCookies"]["Direction"];
+            }
+            else
+            {
+                SortColumn = "Firstname";
+                Direction = "desc";
+                Save();
+            }
+        }
+        public void Save()
+        {
+            HttpContext.Current.Response.Cookies["StateCookies"]["SortColumn"] = SortColumn;
+            HttpContext.Current.Response.Cookies["StateCookies"]["Direction"] = Direction;
+        }
+        public void ColumnChange(string NewColumn)
+        {
+            if (SortColumn == NewColumn)
+            {
+                if (Direction == "desc")
+                {
+                    Direction = "asc";
+                }
+                else
+                {
+                    Direction = "desc";
+                }
+            }
+            else
+            {
+                Direction = "asc";
+            }
+            SortColumn = NewColumn;
+            Save();
+        }
     }
 }
