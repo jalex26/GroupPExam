3fb87678d40c74800cd8a7056f28344efbc6fa2b
Nupur123 <nupur.singh@robertsoncollege.net>
2014-11-09 23:37:30 -0600

    Errors Fixed and Design Changes

--- GroupProject/Master.Master.designer.cs
+++ GroupProject/Master.Master.designer.cs
@@ -76,13 +76,13 @@ namespace GroupProject {
         protected global::System.Web.UI.WebControls.Label lblFirstname;
         
         /// <summary>
-        /// btnLogout control.
+        /// lbLogout control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnLogout;
+        protected global::System.Web.UI.WebControls.LinkButton lbLogout;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
