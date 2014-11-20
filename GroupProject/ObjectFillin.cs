4472435e004253ff38661c2cebde6677c3670e76
kvncoliat <kvncoliat@gmail.com>
2014-11-20 14:34:47 -0600

    changes

---
+++ GroupProject/ObjectFillin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupProject
+{
+    public class ObjectFillin : ObjectParentQuiz<string>
+    {
+        public string _Question { get; set; }
+        public List<string> _Correct { get; set; }
+        public List<string> _Wrong { get; set; }
+
+        public ObjectFillin()
+        {
+            //constructor
+        }
+
+        public ObjectFillin(int QuestionId,string Questi, List<string> Correct, List<string> Wrong)
+        {
+            this._QuestionId = QuestionId;
+            this._Question = Questi;
+            this._Correct = Correct;
+            this._Wrong = Wrong;
+        }
+    }
+}
